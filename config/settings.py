96922cba51665fe944806a0083d76916de03a189
muhammad Ashique V <ashiquekavanoor009@gmail.com>
2026-07-13 10:15:18 +0530

    Fix SECRET_KEY environment variable reference

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv('django-insecure-jqofmgk^&$1cro_352h0atun^6m+ho183e=g#3i0_w#821!gbo')
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
